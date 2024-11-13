f17a982888301bb11ea96698e38a32bc817dcb0c
dotPast <83116565+dotPast@users.noreply.github.com>
2024-11-13 09:34:04 +0300

    replace title image in game

--- pack.py
+++ pack.py
@@ -5,3 +5,12 @@ from PIL import Image
 def setup_pack(ctx: Context):
     ctx.assets.description = 'The "M" Resource Pack. But Worse.'
     ctx.assets.icon = PngFile(source_path="./assets/icon.png")
+
+
+def replace_title(ctx: Context):
+    ctx.assets["minecraft:gui/title/minecraft"] = Texture(
+        Image.open("./assets/title.png")
+    )
+    ctx.assets["minecraft:gui/title/minceraft"] = Texture(
+        Image.open("./assets/title.png")
+    )
