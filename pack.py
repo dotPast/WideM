ca229e2c40241219ff8e10b572087137921cf56e
dotPast <83116565+dotPast@users.noreply.github.com>
2024-11-13 09:14:40 +0300

    remove the replace_stone func

--- pack.py
+++ pack.py
@@ -5,9 +5,3 @@ from PIL import Image
 def setup_pack(ctx: Context):
     ctx.assets.description = 'The "M" Resource Pack. But Worse.'
     ctx.assets.icon = PngFile(source_path="./assets/icon.png")
-
-
-def replace_stone(ctx: Context):
-    ctx.assets["minecraft:block/stone"] = Texture(
-        Image.new("RGB", (16, 16), (256, 0, 0))
-    )
