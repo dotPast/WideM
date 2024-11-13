1a6938aa4e6698ddf6a69a805f3801bc22ea7d86
dotPast <83116565+dotPast@users.noreply.github.com>
2024-11-13 21:39:38 +0300

    move title screen textures to textures/misc/

--- misc.py
+++ misc.py
@@ -4,13 +4,13 @@ from PIL import Image
 
 def title(ctx: Context):
     ctx.assets["minecraft:gui/title/minecraft"] = Texture(
-        Image.open("./assets/title.png")
+        Image.open("./assets/textures/misc/title.png")
     )
     ctx.assets["minecraft:gui/title/minceraft"] = Texture(
-        Image.open("./assets/title.png")
+        Image.open("./assets/textures/misc/title.png")
     )
     ctx.assets["minecraft:gui/title/edition"] = Texture(
-        Image.open("./assets/edition.png")
+        Image.open("./assets/textures/misc/edition.png")
     )
 
 
