2a435d56d88a9e1c29f3d18ebd3ad31b3f87c433
dotPast <83116565+dotPast@users.noreply.github.com>
2024-11-13 08:58:54 +0300

    split setup_pack into setup_pack & replace_stone

--- pack.py
+++ pack.py
@@ -5,6 +5,8 @@ from PIL import Image
 def setup_pack(ctx: Context):
     ctx.assets.description = 'The "M" Resource Pack. But Worse.'
 
+
+def replace_stone(ctx: Context):
     ctx.assets["minecraft:block/stone"] = Texture(
         Image.new("RGB", (16, 16), (256, 0, 0))
     )
