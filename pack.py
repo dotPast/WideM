f214918eadb69dfd3bbe2abcd6ef52a3ec0e0709
dotPast <83116565+dotPast@users.noreply.github.com>
2024-11-13 08:52:21 +0300

    create a beet project

---
+++ pack.py
@@ -0,0 +1,10 @@
+from beet import Context, Texture
+from PIL import Image
+
+
+def setup_pack(ctx: Context):
+    ctx.assets.description = 'The "M" Resource Pack. But Worse.'
+
+    ctx.assets["minecraft:block/stone"] = Texture(
+        Image.new("RGB", (16, 16), (256, 0, 0))
+    )
